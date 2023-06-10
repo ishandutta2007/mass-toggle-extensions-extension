70c6a401ea7774c7243e714785ef561e7f2f911f
ishan dutta <ishandutta2007@gmail.com>
2023-06-11 02:29:01 +0530

    2nd commit

--- src/popup.tsx
+++ src/popup.tsx
@@ -33,6 +33,7 @@ const Popup = () => {
   // }
   return (<div>  COPY THE FOLLOWING:
       <span>
+      {/*
         try {
           let extensions = document.querySelector('extensions-manager').shadowRoot.querySelector('cr-view-manager').querySelector('extensions-item-list').shadowRoot.querySelectorAll("div#container div#content-wrapper div.items-container extensions-item");
           for(let i=0;i<extensions.length;i++) {
@@ -47,6 +48,7 @@ const Popup = () => {
           alert("No ext");
           console.log(error);
         }
+        */}
       </span>
   </div>);
 };
